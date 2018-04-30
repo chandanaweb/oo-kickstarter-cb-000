8adbae05a52aa11c19e9f394038a76d4274f372d
chandana M <chandu11288@gmail.com>
2018-04-30 19:11:24 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,13 +1,12 @@
 class Project
-  attr_accessor :title
+  attr_accessor :title, :backers
   def initialize(title)
     @title = title
     @backers =[]
   end
-  def add_backer(backer=Backer)
+  def add_backer(backer)
     @backers << backer
+    backer.back_project(self)
   end
-  def self.backers
-    @backers
-  end
+
 end
