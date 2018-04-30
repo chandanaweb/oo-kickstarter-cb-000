f0289ffcb2bfadbebc53da79bc9a485a50bac759
chandana M <chandu11288@gmail.com>
2018-04-30 18:19:20 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,15 @@
 class Backer
   attr_accessor :name
+
   def initialize(name)
     @name = name
+    @backed_projects = []
+  end
+  def
+  def back_project(project)
+    @@backed_projects << project
+  end
+  def self.backed_projects
+    @backed_projects
   end
 end
